b5c32f7b81b13d26ee0f52ae2e0130c556f6d41a
Jessie Lin <b19950828@gmail.com>
2022-08-06 18:43:32 +0800

    8/6 remove spots

--- src/MapPage/MapArea/ＭapApplication/mapFrame.js
+++ src/MapPage/MapArea/ＭapApplication/mapFrame.js
@@ -3,7 +3,7 @@ import { useState , useEffect , useCallback , useContext } from 'react'
 import { GoogleMap, useLoadScript , Marker } from '@react-google-maps/api';
 import googleMapStyles from './googleMapStyles';
 import { db } from "../../../connection_firebase/connection_firebase.js"
-import { collection , getDocs , query , where } from "firebase/firestore";
+import { collection , getDocs , query , where , limit } from "firebase/firestore";
 import './mapFrame.css'
 import MarkerInfo from '../MarkerInfo/MarkerInfo.js'
 import Loading_effect from '../../../Component/LoadingEffect/loadingEffect';
@@ -69,10 +69,10 @@ const MapFrame = ({ setText , setBack_to_center , filtered_marker , setFiltered_
         let min_lng=Number((center['lng']-range).toFixed(6))
         let max_lat=Number((center['lat']+range).toFixed(6))
         let max_lng=Number((center['lng']+range).toFixed(6))
-        // let get_res = collection(db, "test-source");          // for testing
-        // let res = query(get_res, limit(15))
-        let get_res = collection(db, "source");
-        let res = query(get_res, where("緯度", ">=", min_lat), where("緯度", "<=", max_lat));  
+        let get_res = collection(db, "test-source");          // for testing
+        let res = query(get_res, limit(0))
+        // let get_res = collection(db, "source");
+        // let res = query(get_res, where("緯度", ">=", min_lat), where("緯度", "<=", max_lat));  
         let snapshot = await getDocs(res);
         let i=0
         let result=[]
@@ -96,10 +96,10 @@ const MapFrame = ({ setText , setBack_to_center , filtered_marker , setFiltered_
           min_lng=Number((center['lng']-0.005).toFixed(6))
           max_lat=Number((center['lat']+0.005).toFixed(6))
           max_lng=Number((center['lng']+0.005).toFixed(6))
-          const new_marker = result.filter(item => {
-              return item['經度'] >= min_lng && item['經度'] <= max_lng && item['緯度'] >= min_lat && item['緯度'] <= max_lat
-          });
-          // const new_marker = result         // for testing
+          // const new_marker = result.filter(item => {
+          //     return item['經度'] >= min_lng && item['經度'] <= max_lng && item['緯度'] >= min_lat && item['緯度'] <= max_lat
+          // });
+          const new_marker = result         // for testing
           return new_marker
         })
       }
